7167932d8132192d104e77f217f46b95aebcec5f
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2026-01-20 17:55:20 +0100

    Update stable2512

--- primitives/impl-tanssi-pallets-config-test/src/lib.rs
+++ primitives/impl-tanssi-pallets-config-test/src/lib.rs
@@ -137,7 +137,6 @@ impl cumulus_pallet_parachain_system::Config for Runtime {
     type ReservedXcmpWeight = ();
     type CheckAssociatedRelayNumber = RelayNumberMonotonicallyIncreases;
     type ConsensusHook = ConsensusHook;
-    type SelectCore = cumulus_pallet_parachain_system::DefaultCoreSelector<Runtime>;
     type RelayParentOffset = ConstU32<0>;
 }
 
